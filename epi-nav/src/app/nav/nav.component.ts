5ad08761809763c22b6382303a19e1fcc2a4a516
breab <brea.borlas@gmail.com>
2018-05-30 16:19:32 -0700

    Add css for lis

--- epi-nav/src/app/nav/nav.component.ts
+++ epi-nav/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseService } from './../course.service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { Course } from './../models/course.model';
 
 
 @Component({
